4c13e068516ad8ebd4b4cfdb9065d3c595256284
Navneel Nandran <navneel.nandran@outlook.com>
2024-03-26 23:59:30 -0400

    add controllers folder, add controller for one warehous

---
+++ contollers/warehouse-controller.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+
+const findUser = async (req, res) => {
+    try {
+        const warehousefound = await knex('warehouses')
+            .where({ id: req.params.id });
+        if (warehousefound === 0) {
+            return res.status(404).json({
+                message: `Warehouse with ID ${req.params.id} not found`
+            });
+        }
+        const warehouseData = warehousefound[0];
+        res.json(warehouseData);
+    } catch (err) {
+        res.status(500).json({
+            message: `Unable to retrieve user data for user with ID ${req.params.id}`,
+        });
+    }
+}
+
+module.export = {
+    findUser,
+}
