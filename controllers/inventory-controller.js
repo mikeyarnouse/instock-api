362d71af34c01e4443b1cef7ef5ef448f352b282
Jake Craig <jtcraig513@gmail.com>
2024-03-27 13:06:17 -0400

    Created the add function in the inventory-coller

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -1,44 +1,88 @@
 const knex = require("knex")(require("../knexfile"));
 
 const index = async (_req, res) => {
-  try {
-    const data = await knex("inventories").select(
-      "id",
-      "warehouse_id",
-      "item_name",
-      "description",
-      "status",
-      "quantity"
-    );
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(400).send(`Error retrieving Inventories: ${error}`);
-  }
+    try {
+        const data = await knex("inventories").select(
+            "id",
+            "warehouse_id",
+            "item_name",
+            "description",
+            "status",
+            "quantity"
+        );
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(400).send(`Error retrieving Inventories: ${error}`);
+    }
 };
 
 const findItem = async (req, res) => {
-  try {
-    const warehouseInventories = await knex("inventories")
-      .join("warehouses", "inventories.warehouse_id", "warehouses.id")
-      .select(
-        "inventories.id",
-        "warehouses.warehouse_name",
-        "inventories.item_name",
-        "inventories.description",
-        "inventories.status",
-        "inventories.quantity"
-      )
-      .where("inventories.id", req.params.id);
+    try {
+        const warehouseInventories = await knex("inventories")
+            .join("warehouses", "inventories.warehouse_id", "warehouses.id")
+            .select(
+                "inventories.id",
+                "warehouses.warehouse_name",
+                "inventories.item_name",
+                "inventories.description",
+                "inventories.status",
+                "inventories.quantity"
+            )
+            .where("inventories.id", req.params.id);
 
-    res.json(warehouseInventories[0]);
-  } catch (error) {
-    res.status(500).json({
-      message: `Unable to retrieve item data for item with ID ${req.params.id}`,
-    });
-  }
+        res.json(warehouseInventories[0]);
+    } catch (error) {
+        res.status(500).json({
+            message: `Unable to retrieve item data for item with ID ${req.params.id}`,
+        });
+    }
 };
 
+const add = async(req, res) => {
+    //Long conditional that just checks that every required field is filled out
+    if (!req.body.warehouse_id || !req.body.item_name || !req.body.description
+        || !req.body.category || !req.body.status || !req.body.quantity) {
+        return res.status(400).json({
+            message: "Please provide necessary details for the inventory item in the request",
+        });
+    }else if (isNaN(req.body.quantity)) {    //Checks if quantity is a valid number. Returns false if not a number
+        return res.status(400).json({
+            message: "Quantity is not a valid number",
+        });
+    }
+
+    // Code to check if warehouse exists
+    const existingWarehouse = await knex("warehouses").where({id: req.body.warehouse_id})
+    if (existingWarehouse.length===0) {
+        return res.status(400).json({
+            message: "Please provide a valid warehouse_id",
+        });
+    }
+
+    try {
+        const result = await knex("inventories").insert(req.body);   //Adds an item to the db. Only reaches here with valid fields
+
+        const newInventoryId = result[0];  //Takes the newly generated id for the item
+        const createdInventoryItem = await knex("inventories").where({id: newInventoryId})//Retrieves the newly created item to return
+        .select(
+            "id",
+            "warehouse_id",
+            "item_name",
+            "description",
+            "category",
+            "status",
+            "quantity"
+        );
+        res.status(201).json(createdInventoryItem);
+    } catch (error) {
+        res.status(500).json({
+            message: `Unable to create new inventory item: ${error}`,
+          });
+    }
+}
+
 module.exports = {
-  index,
-  findItem,
+    index,
+    findItem,
+    add,
 };
--- routes/inventory-routes.js
+++ routes/inventory-routes.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const inventoryController = require('../controllers/inventory-controller');
 
-router.route('/').get(inventoryController.index);
+router
+    .route('/')
+    .get(inventoryController.index)
+    .post(inventoryController.add);
 router.route('/:id').get(inventoryController.findItem);
 
 
